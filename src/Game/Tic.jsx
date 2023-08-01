a356a58a0b2e0097592a337399dbad26988c57e7
MuraliMuraliP <murali.p@pyramidions.in>
2023-08-01 14:49:34 -0700

    games

---
+++ src/Game/Tic.jsx
@@ -0,0 +1,32 @@
+
+const Tic=()=>{
+    return(
+        <>
+<body>
+<h1 className="header">Tic Tac Toe</h1>
+   
+   
+    <div className="row">
+       <div className="num">X</div>
+       <div className="num">O</div>
+       <div className="num">O</div>
+</div>
+
+    <div className="coloumn">
+        <div className="num">O</div>
+        <div className="num">X</div>
+        <div className="num">O</div>
+    </div>
+
+ <div className="row">
+    <div className="num">O</div>
+    <div className="num">O</div>
+    <div className="num">X</div>
+    
+    </div>
+    
+</body>
+</>
+    );
+} 
+export default Tic;
